eaf26d2a02d4862ea27b27a4733a078341ebbcbe
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-09-25 14:02:13 +0530

    Update builds.py

--- builds.py
+++ builds.py
@@ -6,7 +6,18 @@ import subprocess
 import docker
 import json
 
+print("setting the docker client")
+try:
+  client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+  print("docker client set")
+except (APIError, DockerException, ReadTimeout):
+  print("error creating docker client")
+  
+client.images.pull('nginx')
+client.images.pull('ubuntu')
+image_list = client.images.list()
+for item in image_list:
+  print(f"image: {item}")
+container = client.containers.run("ubuntu:latest", "echo hello world")
 
-client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-client.containers.run("ubuntu:latest", "echo hello world")
-client.containers.list()
+
