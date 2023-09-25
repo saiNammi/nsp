80edf31427636d8ad83c0257746e2d9b251ebb9b
saiNammi <118737990+saiNammi@users.noreply.github.com>
2023-09-25 10:51:44 +0530

    Update builds.py

--- builds.py
+++ builds.py
@@ -7,28 +7,6 @@ import docker
 import json
 
 
-def trigger_script_validation_checks(image_name = "registry.access.redhat.com/ubi8/ubi:8.7"):
-    # Spawn a container and pass the build script
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    container = client.containers.run(
-        image_name,
-        "pwd",
-        network = 'host',
-        detach = True,
-        volumes = {
-            current_dir : {'bind': '/home/tester', 'mode': 'rw'}
-        },
-        stderr = True, # Return logs from STDERR
-    )
-    result = container.wait()
-    try:
-        print(container.logs().decode("utf-8"))
-    except Exception:
-        print(container.logs())
-    container.remove()
-    if int(result["StatusCode"]) != 0:
-        raise Exception("Build script validation failed!")
-    else:
-        return True
-
-trigger_script_validation_checks()
+client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+client.containers.run("ubuntu:latest", "echo hello world")
+client.containers.list()
